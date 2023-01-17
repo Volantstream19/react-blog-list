963d06fb1c6c7e88d62bc088c566c54c542def96
King Kong <king-kong@gmail.com>
2023-01-17 13:36:31 -0800

    mapped and rendered through blogs array

---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useBlogs } from '../../hooks/useBlogs.js';
+import BlogCard from '../BlogCard/BlogCard';
+
+export default function Main() {
+  const blogs = useBlogs();
+
+  return (
+    <main>
+      {blogs.map((blog) => (
+        <BlogCard key={blog.id} {...blog} />
+      ))}
+    </main>
+  );
+}
