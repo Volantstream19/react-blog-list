b34bb75ff045e0af02aabb983ac64acd83f99af8
King Kong <king-kong@gmail.com>
2023-01-17 15:00:18 -0800

    CI 2

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-// test.skip('fetches the list of blogs', async () => {
-//   render(<App />);
-//   const blog = await screen.findByText(/Maximize Distributed Users/i);
-//   expect(blog).toBeInTheDocument();
-// });
+test('fetches the list of blogs', async () => {
+  render(<App />);
+  const blog = await screen.findByText(/Whiteboard Robust Blockchains/i);
+  expect(blog).toBeInTheDocument();
+});
