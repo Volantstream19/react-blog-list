bf6f377bd4960022518f4d275a58902d11ec1a94
King Kong <king-kong@gmail.com>
2023-01-17 14:04:11 -0800

    got rid of console logs

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,7 +4,6 @@ import BlogCard from '../BlogCard/BlogCard';
 
 export default function Main() {
   const blogs = useBlogs();
-  console.log(blogs);
 
   return (
     <main>
--- src/services/blog.js
+++ src/services/blog.js
@@ -2,6 +2,5 @@ import { checkError, client } from './client';
 
 export async function getBlogs() {
   const resp = await client.from('blogs').select('*');
-  console.log(resp);
   return checkError(resp);
 }
